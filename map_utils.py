8a40c18497a3132e6d8ba0d4547fdcf66fd85e8d
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-05-18 17:29:14 -0700

    dungeon generating

--- map_utils.py
+++ map_utils.py
@@ -1,9 +1,22 @@
+from random import randint
+
+
 class Rect:
     def __init__(self, x, y, w, h):
         self.x1 = x
         self.y1 = y
         self.x2 = x + w
         self.y2 = y + h
+
+    def center(self):
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+        return (center_x, center_y)
+
+    def intersect(self, other):
+        # returns true if this rectangle intersects with another
+        return (self.x1 <= other.x2 and self.x2 >= other.x1 and
+                self.y1 <= other.y2 and self.y2 >= other.y1)
 
 
 def create_room(game_map, room):
@@ -26,11 +39,52 @@ def create_v_tunnel(game_map, y1, y2, x):
         game_map.transparent[x, y] = True
 
 
-def make_map(game_map):
-    # make two demo rooms
-    room1 = Rect(20, 15, 10, 15)
-    room2 = Rect(35, 15, 10, 15)
+def make_map(game_map, max_rooms, room_min_size, room_max_size, map_width, map_height, player):
 
-    create_room(game_map, room1)
-    create_room(game_map, room2)
-    create_h_tunnel(game_map, 25, 40, 23)
+    rooms = []
+    num_rooms = 0
+
+    for r in range(max_rooms):
+        # random width and height
+        w = randint(room_min_size, room_max_size)
+        h = randint(room_min_size, room_max_size)
+        # random position within map boundaries
+        x = randint(0, map_width - w - 1)
+        y = randint(0, map_height - h - 1)
+
+        # Rect class makes rectangles easier to use
+        new_room = Rect(x, y, w, h)
+
+        # check other rooms for intersections
+        for other_room in rooms:
+            if new_room.intersect(other_room):
+                break
+
+        else:
+            create_room(game_map, new_room)
+
+            # center coordinates of new room, for later
+            (new_x, new_y) = new_room.center()
+
+            if num_rooms == 0:
+                # this is the first room, where the player starts at
+                player.x = new_x
+                player.y = new_y
+            else:
+                # all rooms after the first are connected to the previous one by tunnel
+
+                # center of previous room
+                (prev_x, prev_y) = rooms[num_rooms -1].center()
+
+                # flip a coin
+                if randint(0, 1) == 1:
+                    # horizontal then vertical
+                    create_h_tunnel(game_map, prev_x, new_x, prev_y)
+                    create_v_tunnel(game_map, prev_y, new_y, new_x)
+                else:
+                    # vertical then horizontal
+                    create_v_tunnel(game_map, prev_y, new_y, prev_x)
+                    create_h_tunnel(game_map, prev_x, new_x, new_y)
+
+            rooms.append(new_room)
+            num_rooms += 1
