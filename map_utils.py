949abc479d9040b97f4107d4afcac6e858d29462
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-05-18 17:00:37 -0700

    dummy rooms generated

--- map_utils.py
+++ map_utils.py
@@ -1,11 +1,36 @@
-def make_map(game_map):
-    for x, y in game_map:
+class Rect:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
+
+
+def create_room(game_map, room):
+    # go through the tiles in the room and make them passable
+    for x in range(room.x1 + 1, room.x2):
+        for y in range(room.y1 + 1, room.y2):
+            game_map.walkable[x, y] = True
+            game_map.transparent[x, y] = True
+
+
+def create_h_tunnel(game_map, x1, x2, y):
+    for x in range(min(x1, x2), max(x1, x2) + 1):
         game_map.walkable[x, y] = True
         game_map.transparent[x, y] = True
 
-    game_map.walkable[30, 22] = False
-    game_map.transparent[30, 22] = False
-    game_map.walkable[31, 22] = False
-    game_map.transparent[31, 22] = False
-    game_map.walkable[32, 22] = False
-    game_map.transparent[32, 22] = False
+
+def create_v_tunnel(game_map, y1, y2, x):
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        game_map.walkable[x, y] = True
+        game_map.transparent[x, y] = True
+
+
+def make_map(game_map):
+    # make two demo rooms
+    room1 = Rect(20, 15, 10, 15)
+    room2 = Rect(35, 15, 10, 15)
+
+    create_room(game_map, room1)
+    create_room(game_map, room2)
+    create_h_tunnel(game_map, 25, 40, 23)
